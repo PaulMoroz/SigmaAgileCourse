04f21c004790c71b739d3a07b3c7d8225237c8c0
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-02-12 09:31:57 +0200

    Add files via upload

--- Recognizer/main.py
+++ Recognizer/main.py
@@ -7,6 +7,9 @@ from table import Table
 from table_main import find_table
 import cv2 as cv
 import numpy as np
+import os
+
+
 #=============================
 #RECOGNIZING TEXT ON THE IMAGE
 #=============================
@@ -15,85 +18,125 @@ def transform(image,to):
 		img = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 		im_pil = Image.fromarray(img)
 		return im_pil
-	else:
+	elif to=="cv2":
 		opencvImage = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
 		return opencvImage
 
+
 def recognize(image):
-	#,config='-c preserve_interword_spaces=1',config='--psm = 10'
+	print("Recognizing",image)
 	return tess.image_to_string(image)
  
 
- #Splitting images to table and no-table
 
+#======================================
+#Splitting images to table and no-table
+#======================================
 class HTML2PDF(FPDF, HTMLMixin):
     pass
 
-def image_split(image_path):
-	image = Image.open(image_path)
-	width,height = image.size
+
+#=======================
+#Crop image
+#=======================
+def crop(image):
 	cropped = []
-	tabs = find_table(cv.imread(image_path))
+	width,height = image.size
+	tabs = find_table(transform(image,"cv2"))
 	tabs.insert(0,Table(0,0,0,0)) 
 	tabs.append(Table(0,height,0,0))
-	print(len(tabs))
 	for i in range(0,len(tabs)-1):
-		cropped.append(image.crop((tabs[i].x,tabs[i].y,tabs[i].x+tabs[i].w,tabs[i].y+ tabs[i].h)))
-		cropped.append(image.crop((0,tabs[i].y+tabs[i].h,width,tabs[i+1].y)))
+		cropped.append([image.crop((tabs[i].x,tabs[i].y,tabs[i].x+tabs[i].w,tabs[i].y+ tabs[i].h)),tabs[i]])
+		cropped.append([image.crop((0,tabs[i].y+tabs[i].h,width,tabs[i+1].y)),"common"])
+	cropped.append('eof')
 	cropped = cropped[1:]
-	tabs = tabs[1:]
-	return cropped,tabs 
+	print("Cropped file",len(cropped))
+	return cropped 
 
+
+#=======================
+#Crop some images
+#=======================
+
+
+def image_split(image_path):
+	cropped_all = []
+	if sys.argv[4]=='file':
+		if image_path.endswith("png") or image_path.endswith("jpg"):
+			cropped_all.extend(crop(Image.open(image_path)))
+		else:
+			print("Error!")
+	else:
+		for file in os.listdir(image_path):
+			if file.endswith("png") or file.endswith("jpg"):
+				print("Enter file",file)
+				p = image_path+'/'+file
+				cropped_all.extend(crop(Image.open(image_path+'/'+file)))
+	print("Len cropped all", cropped_all)
+	return cropped_all
+
+
+#==========================================
 #CREATING PDF DOCUMENT WITH RECOGNIZED TEXT
-
+#==========================================
 def to_pdf():
 	htmlcode = ""
-	im_pieces,tabs = image_split()
+	im_pieces = image_split(sys.argv[1])
+	pdf = HTML2PDF()
+	index = 1
 	for k in range(len(im_pieces)):
-		print("k//2 ",k//2," k ",k)
-		if k%2==0 :
-			doc.add_paragraph(recognize(im_pieces[k]))
+		if im_pieces[k] == 'eof':
+			if sys.argv[5] == 'm' or k == len(im_pieces)-1:
+				pdf = HTML2PDF()
+				pdf.add_page()
+				pdf.write_html(htmlcode,'F')
+				pdf.output(str(sys.argv[2])+(str(index) if sys.argv[5] == 'm' else "")+".pdf")
+				index+=1
+				htmlcode = ""
+		elif im_pieces[k][1] == "common":
+			htmlcode += "<p>" + str(recognize(im_pieces[k][0])) + "</p>"
 		else:
-			table_entries = tabs[k//2].get_table_entries()
-			htmlcode += "<p>" + (recognize(im_pieces[k])) + "</p>"
-			print("Rows, cols",len(table_entries),len(table_entries[0]))
+			table_entries = im_pieces[k][1].get_table_entries()
 			htmlcode += "<table>"
-			table_roi = transform(im_pieces[k],"cv")
-			table_roi = cv.resize(table_roi, (tabs[k//2].w * 3, tabs[k//2].h * 3))
-			
+			table_roi = transform(im_pieces[k][0],"cv2")
+			table_roi = cv.resize(table_roi, (im_pieces[k][0].w * 3, im_pieces[k][0].h * 3))
 			try:
 				for i in range(len(table_entries)):
 					row = table_entries[i]
+					htmlcode += "<tr>"
 					for j in range(len(row)):  
 						entry = row[j]
 						entry_roi = table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3]
-						htmlcode += "<td> " + str(recognize(transform(table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3],"PIL"))) + "</td>"
+						htmlcode += "<td " + "width = "+str(100//row)+"% > " + str(recognize(transform(table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3],"PIL"))) + "</td>"
+					htmlcode+="</tr>"
 			except:
 				print("Table Entries" ,len(table_entries))
 			htmlcode+="</table>"
-	pdf = HTML2PDF()
-	pdf.add_page()
-	pdf.write_html(htmlcode)
-	pdf.output(str(sys.argv[2])+".pdf")
 
 
+
+#===========================================
 #CREATING WORD DOCUMENT WITH RECOGNIZED TEXT
-
+#===========================================
 def to_word():
 	doc = Document()
 	im_pieces,tabs = image_split(sys.argv[1]) 
 	print("Lenght of image pieces",len(im_pieces))
-	for k in range(len(im_pieces)):
-		print("k//2 ",k//2," k ",k)
-		if k%2==0 :
-			doc.add_paragraph(recognize(im_pieces[k]))
+	index = 1
+	for k in range(t):
+		if im_pieces[k] == 'eof':
+			if sys.argv[5] == 'm' or k == len(im_pieces)-1:
+			    doc.save(str(sys.argv[2])+str(index)+'.docx' if sys.argv[5]=='m' else str(sys.argv[2])+".docx")
+			    doc = Document()
+			    index+=1
+		elif im_pieces[k][1] == "common" :
+			doc.add_paragraph(recognize(im_pieces[k][0]))
 		else:
-			table_entries = tabs[k//2].get_table_entries()
+			table_entries = im_pieces[k][1].get_table_entries()
 			print("Rows, cols",len(table_entries),len(table_entries[0]))
 			table = doc.add_table(rows = len(table_entries),cols =  len(table_entries[0]))
-			table_roi = transform(im_pieces[k],"cv")
-			table_roi = cv.resize(table_roi, (tabs[k//2].w * 3, tabs[k//2].h * 3))
-			
+			table_roi = transform(im_pieces[k][0],"cv2")
+			table_roi = cv.resize(table_roi, (im_pieces[k][0].w * 3, im_pieces[k][0].h * 3))
 			try:
 				for i in range(len(table_entries)):
 					row = table_entries[i]
@@ -102,25 +145,31 @@ def to_word():
 						entry_roi = table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3]
 						table.cell(i,j).text = str(recognize(transform(table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3],"PIL")))
 			except:
-				print("Table Entries" ,len(table_entries))
-	doc.save(str(sys.argv[2])+".docx")
-	
+				print("Table Entries" ,len(table_entries))	
 
+
+
+#==========================================
 #CREATING TXT DOCUMENT WITH RECOGNIZED TEXT
-
-
+#==========================================
 def to_txt():
-	f = open(str(sys.argv[2])+".txt","w+")
 	im_pieces,tabs = image_split(sys.argv[1])
+	t,index = len(im_pieces),1
+	f = open(str(sys.argv[2])+(str(index) if sys.argv[5] == 'm' else "")+".txt","w+")
 	for k in range(0,len(tabs)):
-		if k%2==0 :
-			f.write(recognize(im_pieces[k]))
+		if im_pieces[k] == 'eof':
+			if sys.argv[5] == 'm':
+				f.close()
+				index+=1
+				f = open(str(sys.argv[2])+str(index)+".txt","w+")
+			elif sys.argv[k] == len(im_pieces) - 1:
+				f.close()
+		if im_pieces[k][1] == "common":
+			f.write(recognize(im_pieces[k][0]))
 		else:
-			table_entries = tabs[k//2].get_table_entries()
-			print("Rows, cols",len(table_entries),len(table_entries[0]))
-			table = doc.add_table(rows = len(table_entries),cols =  len(table_entries[0]))
-			table_roi = transform(im_pieces[k],"cv")
-			table_roi = cv.resize(table_roi, (tabs[k//2].w * 3, tabs[k//2].h * 3))
+			table_entries = im_pieces[k][1].get_table_entries()
+			table_roi = transform(im_pieces[k][0],"cv2")
+			table_roi = cv.resize(table_roi, (im_pieces[k][0].w * 3, im_pieces[k][0].h * 3))
 			try:
 				for i in range(len(table_entries)):
 					row = table_entries[i]
@@ -128,18 +177,17 @@ def to_txt():
 						entry = row[j]
 						entry_roi = table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3]
 						f.write( str(recognize(transform(table_roi[entry[1] * 3: (entry[1] + entry[3]) * 3, entry[0] * 3:(entry[0] + entry[2]) * 3],"PIL"))) + "\t")
-					fwrite("\n")
+					f.write("\n")
 			except:
 				print("Table Entries" ,len(table_entries))
-	f.write(recognize(Image.open(sys.argv[1])))
-	f.close()
+
 
 
 if __name__ == "__main__":
-    if sys.argv[3] == "pdf":
-	to_pdf()
-    elif sys.argv[3] == "docx":
-	to_word()
-    else:
-	to_txt()
+	if sys.argv[3] == "pdf":
+		to_pdf()
+	elif sys.argv[3] == "docx":
+		to_word()
+	else:
+		to_txt()
 
